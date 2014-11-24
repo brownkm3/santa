ad3a71dee5df39b29c0ec4d5c0f67aa5003d6225
Katie Brown <katie.brown@bt.com>
2014-11-24 11:29:49 +0000

    Create spec for basic child object

---
+++ spec/models/child_spec.rb
@@ -0,0 +1,11 @@
+# spec/models/contact.rb
+require 'spec_helper'
+
+describe Child do
+  it "has a valid factory"
+  it "is invalid without a firstname"
+  it "has a lastname"
+  it "is invalid without a good? status"
+  it "returns 'nice' if good? status is true"
+  it "returns 'naughty' if good? status is false"
+end
